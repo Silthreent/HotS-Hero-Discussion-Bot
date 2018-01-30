d9a04f10bad97ec097ec9fbfaad04e38295a20f1
Silthreent <s.il3ntscript3r@gmail.com>
2018-01-30 14:53:16 -0500

    Add project files.

---
+++ HeroMainsBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeroMainsBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
